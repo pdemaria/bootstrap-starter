29f83d42d74e092cb65c1dd1a5f8e16acf392143
Peter Demaria <peter.demaria@bluetonemedia.com>
2014-10-30 08:41:01 -0400

    We're getting there...
    Compiled and minified Bootstrap SASS and JS..

---
+++ GruntFile.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        project: {
+            app: [''],
+            assets: ['<%= project.app %>'],
+            css: ['<%= project.assets %>sass/style.scss']
+        },
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    compass: false,
+                    precision: 10
+                },
+                files: {
+                    'bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap.css': '<%= project.css %>'
+                }
+            }
+        },
+        cssmin: {
+            css: {
+                src: 'bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap.css',
+                dest: 'bower_components/bootstrap-sass-official/assets/stylesheets/bootstrap.min.css'
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min.js': ['bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js']
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: '<%= project.assets %>sass/{,*/}*.{scss,sass}',
+                tasks: ['sass:dev', 'cssmin', 'uglify']
+            }
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', [
+        'watch', 'cssmin:css', 'uglify:js']);
+};
