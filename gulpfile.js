0bf5ed9ef1351d1666fb614a607b3a4ac2f8b2aa
Dillon <dillon@bluetonemedia.com>
2016-02-11 13:22:05 -0500

    Basic require files before starting gulp tasks we'll be creating

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Require Gulp and 3rd Party Libraries
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps');
