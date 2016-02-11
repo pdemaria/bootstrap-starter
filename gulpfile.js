ba0773fd821614f1408561d361348cf2e65b836a
Dillon <dillon@bluetonemedia.com>
2016-02-11 13:26:48 -0500

    Gulp task concatScripts with concatenate your JS files into one file, be sure to replace the mock js files inside the gulp.src array with your own. Furthermore, this will provide sourcemaps for your JS files to allow for easier debugging.

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,20 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps');
+
+//Concat JS files into one file
+//Switch .src array with your actual files
+//Makes one file called app.js in this example
+//Also provides source map for easy debugging
+gulp.task("concatScripts", function() {
+  return gulp.src([
+      //Replace these with your own.
+      'js/jquery.js',
+      'js/sticky/jquery.sticky.js',
+      'js/main.js'
+      ])
+  .pipe(maps.init()) //Source map here
+  .pipe(concat('app.js')) //Rename to desired file, be sure to rename elsewhere
+  .pipe(maps.write('./')) //Path relative to your gulp.dest (sibling of app.js)
+  .pipe(gulp.dest('js')); //Your destination folder
+});
